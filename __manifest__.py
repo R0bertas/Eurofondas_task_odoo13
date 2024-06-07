52c5da87ec0275d48d72b4c13c6c0945d2b113d2
Robert <62176158+R0bertas@users.noreply.github.com>
2024-06-07 10:11:03 +0300

    Update manifest

--- __manifest__.py
+++ __manifest__.py
@@ -1,11 +1,17 @@
 {
-    'name': 'Library Documents',
-    # ...
+    'name': 'Dokumentu valdymas',
+    'category': 'Tools',
+        'description': """
+    Dokumentu ir knygu valdymo sistema
+    ==================================
+    Modulis leidzia valdyti ivairius dokumentus ir knygas, iskaitant ju saugojima, prieziura ir atsakingu asmenu priskyrima.
+        """,
+    'version': '1.0',
+    'author' : 'Robertas Kiselis',
     'views': [
         'views/library_doc_views.xml',
         'views/library_doc_menus.xml',
         'security/ir.model.access.csv',
         'security/library_doc_security.xml',
     ],
-    # ...
 }
